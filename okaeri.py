6803254e9b27c00ec4c874d105b1e45cb36f4198
Penaz <penazarea@altervista.org>
2024-05-18 22:06:27 +0200

    Review, remaster, minimimalized for my needs
    Added some new stuff (like flexbox)

--- okaeri.py
+++ okaeri.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+"""
+This is the rendering system for okaeri.
+
+Requires Mako and a links file
+"""
 import sys
 from mako.template import Template
 
-def parse_file(filename="links.oka"):
-    sections = {}
 
-    with open(filename, 'r') as f:
+def parse_file(filename="links.oka"):
+    """
+    Parses the link file
+    """
+    sections = {}
+    with open(filename, 'r', encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
             if line.startswith("#"):
@@ -13,30 +21,43 @@ def parse_file(filename="links.oka"):
                 sections[sect_name] = []
             elif line != "\n":
                 sections[sect_name].append(line[:-1])
-
         return sections
 
+
 def create_links(sections):
+    """
+    Creates a list of links
+    """
     links = []
     for key, val in sections.items():
         for s in val:
-            s = s.split(' ')
+            s = s.split('  ')
             links.append((key, s[0], s[1]))
     return links
 
 
-sections = parse_file()
-zerudas = create_links(sections)
+if __name__ == "__main__":
+    parsed_sections = parse_file()
+    zerudas = create_links(parsed_sections)
 
-# Gets theme
-if len(sys.argv) == 2:
-    theme = sys.argv[1]
+    # Gets theme
+    theme = "train"  # default
+    if len(sys.argv) == 2:
+        theme = sys.argv[1]
 
-else:
-    theme = "train" # default
-
-tmpl = Template(filename='./html/template.html', input_encoding='utf-8', output_encoding='utf-8')
+    tmpl = Template(
+        filename='./html/template.html',
+        input_encoding='utf-8',
+        output_encoding='utf-8'
+    )
 
 # Creates homepage from template
-with open("./html/homepage.html", 'wb') as f_out:
-    f_out.write(bytes(tmpl.render(zerudas=zerudas, sections=sections, theme=theme)))
+    with open("./html/homepage.html", 'wb') as f_out:
+        f_out.write(
+            bytes(
+                tmpl.render(
+                    zerudas=zerudas,
+                    sections=parsed_sections,
+                    theme=theme)
+            )
+        )
